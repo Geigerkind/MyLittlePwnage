84352602ae5b4e13a48bf10cbd8334732fc42826
Lergin <lergin@gmx.de>
2018-03-22 22:45:14 +0100

    added build

---
+++ docs/precache-manifest.60e91ff58cdb7e2e83d808298f4c9e38.js
@@ -0,0 +1,25 @@
+self.__precacheManifest = [
+  {
+    "url": "vendors~firebaseui.bundle.b093bbac09a405760c09.js"
+  },
+  {
+    "url": "top10k.bundle.b093bbac09a405760c09.js"
+  },
+  {
+    "revision": "b9a3c557928766ed8ab6b5560d85d682",
+    "url": "index.html"
+  },
+  {
+    "url": "crypto-js.bundle.b093bbac09a405760c09.js"
+  },
+  {
+    "revision": "f4587898c912ab018cf0",
+    "url": "app.styles.6b5b2c184c28f517384f35e25517a8ac.css"
+  },
+  {
+    "url": "app.bundle.b093bbac09a405760c09.js"
+  },
+  {
+    "url": "3414894f6df05c17c9e848256138e374.png"
+  }
+];
