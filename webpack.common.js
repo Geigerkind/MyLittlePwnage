5a80d004614489df286143f0bc7f497e6514063d
Lergin <lergin@gmx.de>
2018-03-22 20:13:58 +0100

    split to multiple files + webpack

---
+++ webpack.common.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './lib/index.js',
+  },
+  output: {
+    filename: '[name].bundle.[hash].js',
+    path: path.resolve(__dirname, 'docs')
+  },
+  plugins: [
+    new CleanWebpackPlugin(['docs']),
+    new HtmlWebpackPlugin({
+      title: 'MyLittlePwnage',
+      //  hash: true
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+    ]
+  }
+};
