5bb4fd85858ffee723b6949f03b876f31ce1fca4
Geigerkind <tomdymel@web.de>
2018-03-22 21:49:58 +0100

    Added credits and authors

--- index.js
+++ index.js
@@ -427,8 +427,10 @@ const imprintTemplate = state => html`
 <b>Contact: MyLittlePwnage@lergin.de</b>
 <h1>Credits</h1><br />
 <b><a href="https://haveibeenpwned.com">HaveIBeenPwned</a></b>: Provides an useful API to check passwords.<br /><br />
-<b><a href="https://github.com/danielmiessler/SecLists/blob/master/Passwords/darkweb2017-top10K.txt">Daniel Miessler</a></b>: Provided password lists.
-TODO: Giphy thanks
+<b><a href="https://github.com/danielmiessler/SecLists/blob/master/Passwords/darkweb2017-top10K.txt">Daniel Miessler</a></b>: Provided password lists.<br />
+<b><a href="https://giphy.com">Giphy</a></b>: Providing funny gifs.
+<h1>Authors</h1><br />
+Created by Malte Laukötter, Jannik Friemann and Tom Dymel.
 </div>
 `
 
